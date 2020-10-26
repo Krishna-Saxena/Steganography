00c66bf500ec0e543d7968d480a2430e540cedc0
krishna-saxena <krishnasaxena@outlook.com>
2020-10-26 13:38:10 -0700

    Updated folder settings
    Java classes show up correctly

---
+++ src/Encrypter.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+public class Encrypter {
+    Image image;
+    String string;
+
+    public Encrypter(Image image, String string) {
+        this.image = image;
+        this.string = string;
+    }
+
+
+}
