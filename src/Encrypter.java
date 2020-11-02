ce17b0f53a72a3b58125406bfaf2636e9ed81ccb
krishna-saxena <krishnasaxena@outlook.com>
2020-11-02 14:07:37 -0800

    Encrypter changes

--- src/Encrypter.java
+++ src/Encrypter.java
@@ -1,7 +1,7 @@
 import squint.SImage;
 
 public class Encrypter {
-    public static SImage encypt(SImage image, int[] arr) {
+    public static SImage encrypt(SImage image, int[] arr) {
         int width = image.getWidth(), height = image.getHeight();
         int[][] redPixels = image.getPixelArray(SImage.RED);
         int[][] greenPixels = image.getPixelArray(SImage.GREEN);
@@ -20,7 +20,27 @@ public class Encrypter {
         SImage sImage = new SImage(redPixels, greenPixels, bluePixels);
         return sImage;
     }
-    public static SImage stegImage(SImage image, int[] arr) {
+    public static SImage encrypt(SImage image, SImage mask) {
+        if (image.getHeight() != mask.getHeight() || image.getWidth() != mask.getWidth()) {
+            throw new IllegalArgumentException("image and mask must have the same dimensions");
+        }
+        int[][] maskRedPixels = mask.getRedPixelArray();
+        int[][] redPixels = image.getRedPixelArray();
+        int[][] greenPixels = image.getGreenPixelArray();
+        int[][] bluePixels = image.getBluePixelArray();
+
+        for (int r = 0; r < redPixels.length; r++)
+            for (int c =  0; c < redPixels[0].length; c++) {
+                int lastBit = maskRedPixels[r][c] % 2;
+                redPixels[r][c] = redPixels[r][c]/2*2+lastBit;
+                greenPixels[r][c] = greenPixels[r][c]/2*2+lastBit;
+                bluePixels[r][c] = bluePixels[r][c]/2*2+lastBit;
+            }
+
+        return new SImage(redPixels, greenPixels, bluePixels);
+    }
+
+    public static SImage stegMask(SImage image, int[] arr) {
         int width = image.getWidth(), height = image.getHeight();
         int[][] redPixels = new int[width][height];
         int[][] greenPixels = new int[width][height];
