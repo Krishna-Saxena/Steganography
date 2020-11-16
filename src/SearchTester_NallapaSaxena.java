e52e0f0bbe1d73e3062920cba62caec84ab88c2e
krishna-saxena <krishnasaxena@outlook.com>
2020-11-16 14:44:13 -0800

    Recursive bin search

--- src/SearchTester_NallapaSaxena.java
+++ src/SearchTester_NallapaSaxena.java
@@ -10,11 +10,12 @@ public class SearchTester_NallapaSaxena {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		SearchTester_NallapaSaxena st = new SearchTester_NallapaSaxena();
 		// Ask the user for search string
 		Scanner myScan = new Scanner(System.in);
 		System.out.println("Enter name: ");
 		String input = myScan.nextLine();
-		int index = binSearch( getList(), input );
+		int index = st.binSearchWreck( getList(), input );
 		System.out.println(input + " found in list at index: " + index);
 
     }
@@ -30,10 +31,22 @@ public class SearchTester_NallapaSaxena {
     }
     
     public int binSearch(String[] arr, String s){
-
+		return -1;
     }
 
     public int binSearchWreck(String[] arr, String s){
-
+		return recHelper(arr, s, 0, arr.length-1);
     }
+
+    private int recHelper(String[] arr, String s, int lo, int hi) {
+		if (hi < lo || lo < 0 || hi >= arr.length)
+			return -1;
+		int mid = lo+(hi-lo)/2;
+		if (arr[mid].equals(s))
+			return mid;
+		else if (arr[mid].compareTo(s) > 0) // look on left
+			return recHelper(arr, s, lo, mid-1);
+		else
+			return recHelper(arr, s, mid+1, hi);
+	}
 }
